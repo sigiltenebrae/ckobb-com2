5267171029f9f0691d5bce3775dbe194864596a8
sigiltenebrae <ck6202@rit.edu>
2022-08-17 19:45:52 -0400

    theme palette setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, HostBinding, OnInit} from '@angular/core';
+import {OverlayContainer} from "@angular/cdk/overlay";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ckobbcom2';
+export class AppComponent implements OnInit {
+  title = 'chriskobb';
+
+  public isDark = true;
+
+  constructor(public overlayContainer: OverlayContainer) {}
+
+  ngOnInit(): void {
+        this.onSetTheme('dark-theme');
+    }
+
+  public toggleDarkTheme() {
+    if (this.isDark) {
+      this.onSetTheme('light-theme');
+      this.isDark = false;
+    }
+    else {
+      this.onSetTheme('dark-theme');
+      this.isDark = true;
+    }
+  }
+
+  @HostBinding('class') componentCssClass: any;
+  private onSetTheme(theme: any) {
+    this.overlayContainer.getContainerElement().classList.add(theme);
+    this.componentCssClass = theme;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { OverlayModule} from '@angular/cdk/overlay';
+import { RouterModule } from "@angular/router";
+
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatListModule } from "@angular/material/list";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatSliderModule } from "@angular/material/slider";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatCardModule } from "@angular/material/card";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatButtonModule } from "@angular/material/button";
+import { MatChipsModule } from "@angular/material/chips";
+import { MatOptionModule, MatRippleModule } from "@angular/material/core";
+import { MatInputModule } from "@angular/material/input";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    OverlayModule,
+    FlexLayoutModule,
+    RouterModule.forRoot([
+      {path: '', redirectTo: 'projects', pathMatch: 'full'},
+      {path: 'projects', component: ProjectsComponent}
+    ]),
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
